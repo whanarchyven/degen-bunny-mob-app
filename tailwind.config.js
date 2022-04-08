ce571f93a06798af3f9a276c0aa180a0b284c0b9
Ivan Zaburdaev <ivanlom227@gmail.com>
2022-04-08 18:23:13 +0300

    fonts, tailwind configs - prepare for coding

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{ts,js,tsx,jsx}",
+    "./components/**/*.{ts,js,tsx,jsx}",
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'gilroy': ['Gilroy', 'system-ui'],
+    },
+    colors: {
+      purple: "#7D1ABA",
+      aue:"#b03efc",
+      blue: "#3F65EC",
+      semiblue:"#8952FF",
+      white:'#FFFFFF',
+    },
+  },
+  plugins: [],
+}
